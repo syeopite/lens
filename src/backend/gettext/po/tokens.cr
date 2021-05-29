0e290fa95cff0a0fc6aa3a77e199ee61b982f875
syeopite <syeopite@syeopite.dev>
2021-05-29 14:22:31 -0700

    Add mapping of string header to enum header token

--- src/backend/gettext/po/tokens.cr
+++ src/backend/gettext/po/tokens.cr
@@ -3,7 +3,7 @@ require "../../base.cr"
 module Gettext
   extend self
 
-    # Enum of current supported Tokens for Gettext (po)
+  # Enum of current supported Tokens for Gettext (po)
   private enum POTokens
     STRING
     PREV_MSGID
@@ -14,6 +14,8 @@ module Gettext
     MSGSTR_PLURAL_ID
 
     # Headers
+    # Most of this is unnessersary for us. We really only need the plural-forms and
+    # and language header. But just in case we'll tokenize them anyways
     PROJECT_ID_VERSION
     REPORT_MSGID_BUGS_TO
     POT_CREATION_DATE
@@ -27,12 +29,27 @@ module Gettext
   end
 
   # Hash mapping string representation of keywords to their Enum type counterpart.
-  # This is to allow for faster parsing.
+  # This is to allow for faster scanning.
   private KEYWORDS = {
     "msgctxt" => POTokens::MSGCTXT,
     "msgid" => POTokens::MSGID,
     "msgid_plural" => POTokens::MSGID_PLURAL,
     "msgstr" => POTokens::MSGSTR
+  }
+
+  # Hash mapping of string representation of headers to their enum counterpart.
+  # This is to allow for faster scanning them
+  private Headers = {
+    "Project-Id-Version" => POTokens::PROJECT_ID_VERSION,
+    "Report-Msgid-Bugs-To" => POTokens::REPORT_MSGID_BUGS_TO,
+    "POT-Creation-Date" => POTokens::POT_CREATION_DATE,
+    "PO-Revision-Date" => POTokens::PO_REVISION_DATE,
+    "Last-Translator" => POTokens::LAST_TRANSLATOR,
+    "Language-Team" => POTokens::LANGUAGE_TEAM,
+    "Language" => POTokens::LANGUAGE,
+    "Content-Type" => POTokens::CONTENT_TYPE,
+    "Content-Transfer-Encoding" => POTokens::CONTENT_TRANSFER_ENCODING,
+    "Plural-Forms" => POTokens::PLURAL_FORMS,
   }
 
   # Object representing a token from the grammar of gettext po files
