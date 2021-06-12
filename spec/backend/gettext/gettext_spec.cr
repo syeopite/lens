f75225d71b5fbef195ca57f7719f3e51228548a0
syeopite <syeopite@syeopite.dev>
2021-06-12 06:10:01 -0700

    Change gettext spec to validate po file lexing

--- spec/backend/gettext/gettext_spec.cr
+++ spec/backend/gettext/gettext_spec.cr
@@ -1,3 +1,4 @@
+require "digest"
 require "../../../src/backend/gettext"
 
 describe Gettext do
@@ -8,7 +9,7 @@ describe Gettext do
 
   it "Able to tokenlize locale files" do
     new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
-    timeit new_backend_instance.load
-    timeit new_backend_instance.scan
+    new_backend_instance.load
+    Digest::SHA256.hexdigest(new_backend_instance.scan["example.po"].to_s).should eq("dc8c3e87611a63f9eedc7acc12650793834230ddf42a4a2066134aa616fed473")
   end
 end
