8dbf86bf8af9903f63094d7408d3604833cbc663
syeopite <syeopite@syeopite.dev>
2021-07-01 23:31:32 -0700

    Add parser to parse plural-form exprs into ASTs

--- spec/backend/gettext/plural-form_spec.cr
+++ spec/backend/gettext/plural-form_spec.cr
@@ -1,3 +1,4 @@
+require "digest"
 require "../../../src/backend/gettext/plural-forms/*"
 
 describe PluralForm do
@@ -10,6 +11,18 @@ describe PluralForm do
     it "is able to tokenize a complex plural form expressions" do
       plural_form_scanner = PluralForm::Scanner.new("nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);")
       Digest::SHA256.hexdigest(plural_form_scanner.scan.to_s).should eq "fe58796b4c706c1476ee8daee0b8a1f57514811cdbb0e8a8db152b686a0751cf"
+    end
+  end
+
+  describe "Parser" do
+    it "is able to parse a simple plural form expression" do
+      plural_form_scanner = PluralForm::Scanner.new("nplurals=2; plural=(n > 1);")
+      PluralForm::Parser.new(plural_form_scanner.scan).parse
+    end
+
+    it "is able to parse a complex plural form expression" do
+      plural_form_scanner = PluralForm::Scanner.new("nplurals=3; plural=(n%10==1 && n%100!=11 ? 0 : n%10>=2 && n%10<=4 && (n%100<10 || n%100>=20) ? 1 : 2);")
+      PluralForm::Parser.new(plural_form_scanner.scan).parse
     end
   end
 end
