3158722296ce55b7d9d0eb6353350139f245d85b
syeopite <syeopite@syeopite.dev>
2021-07-11 02:25:39 -0700

    Rewrite gettext spec

--- spec/backend/gettext/gettext_spec.cr
+++ spec/backend/gettext/gettext_spec.cr
@@ -2,20 +2,37 @@ require "digest"
 require "../../../src/backend/gettext"
 
 describe Gettext do
-  it "Able to load locale files" do
-    new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
-    new_backend_instance.load
+  describe "po" do
+    it "Able to tokenize po files" do
+      new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
+      new_backend_instance.load
+      Digest::SHA256.hexdigest(new_backend_instance.scan["example.po"].to_s).should eq("5db58843791927dc46ed39427879a33ea659a566394aaf50c7692144e386125c")
+    end
+
+    it "Able to parse and use po files" do
+      new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
+      new_backend_instance.load
+      c = new_backend_instance.parse(new_backend_instance.scan)["example.po"]
+
+      c.gettext("%i line of file “%s” was not loaded correctly.").should eq("%i سطر ملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 0).should eq("%i سطر ملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 40).should eq("%i سطر الملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 100).should eq("%i آسطر الملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.pgettext("column/row header", "Needs Work").should eq("تحتاج عملًا")
+    end
   end
 
-  it "Able to tokenize locale files" do
-    new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
-    new_backend_instance.load
-    Digest::SHA256.hexdigest(new_backend_instance.scan["example.po"].to_s).should eq("5db58843791927dc46ed39427879a33ea659a566394aaf50c7692144e386125c")
-  end
+  describe "mo" do
+    it "Able to parse and use mo files" do
+      new_backend_instance = Gettext::MOBackend.new("spec/backend/gettext/locales")
+      new_backend_instance.load
+      c = new_backend_instance.parse["example.mo"]
 
-  it "Able to parse locale files" do
-    new_backend_instance = Gettext::POBackend.new("spec/backend/gettext/locales")
-    new_backend_instance.load
-    c = new_backend_instance.parse(new_backend_instance.scan)
+      c.gettext("%i line of file “%s” was not loaded correctly.").should eq("%i سطر ملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 0).should eq("%i سطر ملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 40).should eq("%i سطر الملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.ngettext("%i line of file “%s” was not loaded correctly.", "%i lines of file “%s” were not loaded correctly.", 100).should eq("%i آسطر الملف ““%s”” لم يتم تحميله بشكل صحيح.")
+      c.pgettext("column/row header", "Needs Work").should eq("تحتاج عملًا")
+    end
   end
 end
