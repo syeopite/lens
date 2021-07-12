c9abcedcb12f301a760c2ae5c4ea8b7709f7a3a6
syeopite <syeopite@syeopite.dev>
2021-07-12 03:50:37 -0700

    Document gettext Catalogue object

--- src/backend/gettext/gettext.cr
+++ src/backend/gettext/gettext.cr
@@ -4,10 +4,20 @@ module Gettext
   extend self
 
   # Gettext message catalogue. Contains methods for handling translations
+  #
+  # You **should not** be manually creating an instance of this class! Instead let the Gettext backends
+  # do it for you! See `Gettext::MOBackend` and `Gettext::POBackend`
   class Catalogue
+    # Returns a hash of the headers
+    #
+    # ```
+    # catalogue = Gettext::MOBackend.new("examples").create
+    # catalogue.headers["Plural-Forms"] # => "nplurals=2; plural=(n != 1);"
+    # ```
     getter headers : Hash(String, String)
     @plural_interpreter : PluralForm::Interpreter?
 
+    # Creates a message catalogue from parsed Gettext data
     def initialize(@contents : Hash(String, Hash(Int8, String)))
       @headers = {} of String => String
 
@@ -33,7 +43,13 @@ module Gettext
       return @plural_interpreter.not_nil!.interpret(n)
     end
 
-    # Fetch the translated message for the specific ID. If none can be found the given ID is returned.
+    # Fetches the translated message for the specific ID. If none can be found the given ID is returned.
+    #
+    # ```
+    # catalogue = Gettext::MOBackend.new("examples").create
+    # catalogue.gettext("A message")     # => "Translated message"
+    # catalogue.gettext("I don't exist") # => "I don't exist"
+    # ```
     def gettext(id : String)
       begin
         return @contents[id][0]
@@ -43,6 +59,16 @@ module Gettext
     end
 
     # Fetches the translated message for the specific ID with the correct plural form. Returns either the singular or plural id if none can be found.
+    #
+    # ```
+    # catalogue = Gettext::MOBackend.new("examples").create
+    # catalogue.ngettext("I have %d apple", "I have %d apples", 0) # => "Translated message with plural-form 1"
+    # catalogue.ngettext("I have %d apple", "I have %d apples", 1) # => "Translated message with plural-form 0"
+    #
+    # # Not found:
+    # catalogue.ngettext("I have %d pear", "I have %d pears", 0) # => "I have %d pear"
+    # catalogue.ngettext("I have %d pear", "I have %d pears", 1) # => "I have %d pears"
+    # ```
     def ngettext(id : String, plural_id, n)
       if @plural_interpreter.nil?
         return id
@@ -60,6 +86,13 @@ module Gettext
     end
 
     # Fetches the translated message for the specific ID that is bound by context. If none can be found the given ID is returned.
+    #
+    # ```
+    # catalogue = Gettext::MOBackend.new("examples").create
+    #
+    # catalogue.pgettext("CopyPasteMenu", "copy")          # => "Translated copy"
+    # catalogue.pgettext("CopyPasteMenu", "I don't exist") # => "I don't exist"
+    # ```
     def pgettext(context, id)
       begin
         return @contents["#{context}\u0004#{id}"][0]
@@ -69,6 +102,16 @@ module Gettext
     end
 
     # Fetches the translated message for the specific ID that is bound by context with the correct plural form.
+    #
+    # ```
+    # catalogue = Gettext::MOBackend.new("examples").create
+    # catalogue.npgettext("CopyPasteMenu", "Export %d file", "Export %d files", 0) # => "Translated message with plural-form 1"
+    # catalogue.npgettext("CopyPasteMenu", "Export %d file", "Export %d files", 1) # => "Translated message with plural-form 0"
+    #
+    # # Not found:
+    # catalogue.npgettext("CopyPasteMenu", "None", "NonePlural", 0) # => "NonePlural"
+    # catalogue.npgettext("CopyPasteMenu", "None", "NonePlural", 1) # => "None"
+    # ```
     def npgettext(context, id, plural_id, n)
       if @plural_interpreter.nil?
         return id
