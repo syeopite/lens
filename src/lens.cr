07605aa989647e8167c2bf68bb2d6261c25d3f96
syeopite <syeopite@syeopite.dev>
2021-07-14 14:43:22 -0700

    Add new fetch_backend method to main Lens module

--- src/lens.cr
+++ src/lens.cr
@@ -1,8 +1,36 @@
 require "./backend/*"
 
-# TODO: Write documentation for `Lens`
 module Lens
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  # List of supported formats within lens.
+  #
+  # For their respective documentations see:
+  # `Gettext::POBackend`
+  # `Gettext::MOBackend`
+  # `CrystalI18n::I18n`
+  enum Formats
+    GettextPO
+    GettextMO
+    CrystalI18nYAML
+  end
+
+  # Returns the backend for the selected format
+  #
+  # Please note that each backend has a different API for translations.
+  #
+  # ```
+  # require "lens"
+  #
+  # Lens.fetch_backend(Lens::Formats::GettextPO)       # => Gettext::POBackend
+  # Lens.fetch_backend(Lens::Formats::GettextMO)       # => Gettext::MOBackend
+  # Lens.fetch_backend(Lens::Formats::CrystalI18nYAML) # => CrystalI18n::I18n
+  # ```
+  def fetch_backend(fmt : Formats)
+    case fmt
+    when GettextPO       then Gettext::POBackend
+    when GettextMO       then Gettext::MOBackend
+    when CrystalI18nYAML then CrystalI18n::I18n
+    end
+  end
 end
