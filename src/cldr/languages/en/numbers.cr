eb7a1dac08a8ae0e192289da9efd4dce751b3bdc
syeopite <syeopite@syeopite.dev>
2021-08-30 01:04:48 -0700

    Fix typo in CLDR EN number data

--- src/cldr/languages/en/numbers.cr
+++ src/cldr/languages/en/numbers.cr
@@ -21,125 +21,125 @@ module CLDR
 
       DecimalLongFormat = {
         "1000": {
-          "one":    "0 thousand",
-          "other:": "0 thousand",
+          "one":   "0 thousand",
+          "other": "0 thousand",
         },
 
         "10000": {
-          "one":    "00 thousand",
-          "other:": "00 thousand",
+          "one":   "00 thousand",
+          "other": "00 thousand",
         },
 
         "100000": {
-          "one":    "000 thousand",
-          "other:": "000 thousand",
+          "one":   "000 thousand",
+          "other": "000 thousand",
         },
 
         "1000000": {
-          "one":    "0 million",
-          "other:": "0 million",
+          "one":   "0 million",
+          "other": "0 million",
         },
 
         "10000000": {
-          "one":    "00 million",
-          "other:": "00 million",
+          "one":   "00 million",
+          "other": "00 million",
         },
 
         "100000000": {
-          "one":    "000 million",
-          "other:": "000 million",
+          "one":   "000 million",
+          "other": "000 million",
         },
 
         "1000000000": {
-          "one":    "0 billion",
-          "other:": "0 billion",
+          "one":   "0 billion",
+          "other": "0 billion",
         },
 
         "10000000000": {
-          "one":    "00 billion",
-          "other:": "00 billion",
+          "one":   "00 billion",
+          "other": "00 billion",
         },
 
         "100000000000": {
-          "one":    "000 billion",
-          "other:": "000 billion",
+          "one":   "000 billion",
+          "other": "000 billion",
         },
 
         "1000000000000": {
-          "one":    "0 trillion",
-          "other:": "0 trillion",
+          "one":   "0 trillion",
+          "other": "0 trillion",
         },
 
         "10000000000000": {
-          "one":    "00 trillion",
-          "other:": "00 trillion",
+          "one":   "00 trillion",
+          "other": "00 trillion",
         },
 
         "100000000000000": {
-          "one":    "000 trillion",
-          "other:": "000 trillion",
+          "one":   "000 trillion",
+          "other": "000 trillion",
         },
       }
 
       DecimalShortFormat = {
         "1000": {
-          "one":    "0K",
-          "other:": "0K",
+          "one":   "0K",
+          "other": "0K",
         },
 
         "10000": {
-          "one":    "00K",
-          "other:": "00K",
+          "one":   "00K",
+          "other": "00K",
         },
 
         "100000": {
-          "one":    "000K",
-          "other:": "000K",
+          "one":   "000K",
+          "other": "000K",
         },
 
         "1000000": {
-          "one":    "0M",
-          "other:": "0M",
+          "one":   "0M",
+          "other": "0M",
         },
 
         "10000000": {
-          "one":    "00M",
-          "other:": "00M",
+          "one":   "00M",
+          "other": "00M",
         },
 
         "100000000": {
-          "one":    "000M",
-          "other:": "000M",
+          "one":   "000M",
+          "other": "000M",
         },
 
         "1000000000": {
-          "one":    "0B",
-          "other:": "0B",
+          "one":   "0B",
+          "other": "0B",
         },
 
         "10000000000": {
-          "one":    "00B",
-          "other:": "00B",
+          "one":   "00B",
+          "other": "00B",
         },
 
         "100000000000": {
-          "one":    "000B",
-          "other:": "000B",
+          "one":   "000B",
+          "other": "000B",
         },
 
         "1000000000000": {
-          "one":    "0T",
-          "other:": "0T",
+          "one":   "0T",
+          "other": "0T",
         },
 
         "10000000000000": {
-          "one":    "00T",
-          "other:": "00T",
+          "one":   "00T",
+          "other": "00T",
         },
 
         "100000000000000": {
-          "one":    "000T",
-          "other:": "000T",
+          "one":   "000T",
+          "other": "000T",
         },
       }
 
