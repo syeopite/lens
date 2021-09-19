46911dc04d5b2b736ee747f488a7cc4b5e105e6c
syeopite <syeopite@syeopite.dev>
2021-09-18 22:12:37 -0700

    Add fractional_*_grouping metadata attributes

--- src/cldr/logic/numbers/rules.cr
+++ src/cldr/logic/numbers/rules.cr
@@ -26,8 +26,13 @@ module CLDR::Numbers
     {% for method in %w(setter getter) %}
       {{method.id}} secondary_grouping : Int32?
       {{method.id}} primary_grouping : Int32?
+
+      {{method.id}} fractional_secondary_grouping : Int32?
+      {{method.id}} fractional_primary_grouping : Int32?
+
       {{method.id}} maximum_significant_figures : Int32?
       {{method.id}} minimum_significant_figures : Int32?
+
       {{method.id}} use_padding : Bool
       {{method.id}} padding_character : String?
 
@@ -38,6 +43,8 @@ module CLDR::Numbers
     def initialize
       @secondary_grouping = nil
       @primary_grouping = nil
+      @fractional_secondary_grouping = nil
+      @fractional_primary_grouping = nil
       @maximum_significant_figures = nil
       @minimum_significant_figures = nil
       @use_padding = false
